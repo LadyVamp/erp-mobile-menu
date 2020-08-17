8768c048e62f343b053add179231b83a8d57f683
Дьякова Надежда <n.dyakova@voskhod.ru>
2020-08-17 19:09:27 +0300

    [+] get data from service

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # ErpMobileMenu
 Меню для кафешки, мобильная версия
+
 [Макет](https://www.figma.com/file/Onajg6eSbpwGvZrTjTbvjT/ERP---menu)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-
+import { AppService } from './app.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor() { }
+  jsonData: any;
+
+  constructor(private service: AppService) { }
+
+  ngOnInit() {
+    this.service.getData()
+      .subscribe((data: any): void => {
+        console.info(data);
+        this.jsonData = data;
+      });
+  }
+
 }
 
